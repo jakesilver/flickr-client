a2fdace60b6f1e86f2875cb40d89638a31634b8e
Jake Silver <jakesilver@Jakes-Mac-mini.local>
2023-06-08 22:53:16 -0500

    take-home: Set up photo summaries view model

---
+++ app/src/main/java/com/jakesilver/takehome/scintillate/PhotoViewModel.kt
@@ -0,0 +1,19 @@
+package com.jakesilver.takehome.scintillate
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.jakesilver.takehome.api.FlickrRepository
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+class PhotoViewModel(
+    private val savedStateHandle: SavedStateHandle,
+    private val repository: FlickrRepository
+) : ViewModel() {
+
+    private var searchTag: String? = savedStateHandle["tag"]
+
+    val photoSummaries = repository.getPhotoResultsStream(searchTag ?: "").cachedIn(viewModelScope)
+}
