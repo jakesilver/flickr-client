b744209dc6ce52fe8297f97a0e46d5888c41d8f3
Jake Silver <jakesilver03@gmail.com>
2025-07-06 19:10:28 -0500

    fix koin upgrade deprecation warning

--- app/src/main/java/com/jakesilver/photoclient/scintillate/di/AppModule.kt
+++ app/src/main/java/com/jakesilver/photoclient/scintillate/di/AppModule.kt
@@ -2,7 +2,7 @@ package com.jakesilver.photoclient.scintillate.di
 
 import com.jakesilver.photoclient.scintillate.viewmodels.PhotoDetailsViewModel
 import com.jakesilver.photoclient.scintillate.viewmodels.PhotoSearchViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
